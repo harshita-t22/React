500eae38e837a79eec4213da58a4a0d694aedea2
harshitaa2205 <23f1000768@ds.study.iitm.ac.in>
2025-07-08 17:37:13 +0530

    initial hello msg

---
+++ App.js
@@ -0,0 +1,23 @@
+const parent= React.createElement("div",{id:"parent"},[
+    React.createElement(
+    "div",
+    {id:"child1"},
+    [React.createElement("h1",{},"I m h1 tag"),
+    React.createElement("h2",{},"I m h2 tag"),
+    ]),
+    React.createElement(
+    "div",
+    {id:"child2"},
+    [React.createElement("h1",{},"I m h1 tag"),
+    React.createElement("h2",{},"I m h2 tag"),
+    ]),
+]);
+
+
+
+const heading= React.createElement(
+    "h1",
+    {id:"heading", xyz: "abc"},
+    "Hello World from React!"); /*create object that root.render convets into html tag */
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
